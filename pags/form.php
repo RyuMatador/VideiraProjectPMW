5abfeb9d1fd8a25fcc6bced32a9b649904dd8561
RyuMatador <sueliodez@hotmail.com>
2018-12-19 10:52:53 -0300

    Atualização Geral: Incorporação do HTML ao WordPress

--- pags/form.php
+++ pags/form.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Videira Palmas</title>
-  <link rel="shortcut icon" href="../icons/favicon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-    crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
-    crossorigin="anonymous">
-  <link rel="stylesheet" href="../styles/form-style.css">
-</head>
-
-<body>
+<?php get_header(); ?>
   <!-- NAVIGATION -->
   <nav class="navbar navbar-expand-md nav-shadow fixed-top m-0 py-0 navbar-style">
     <a href="../index.html" class="navbar-brand">
@@ -215,24 +199,4 @@
         </div>
   </section>
 
-  <!-- COPYRIGHT -->
-  <footer>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 bg-primary text-center text-white py-2 align-items-center">
-          <p class="mb-0">Videira Palmas 2019 © - Todos os direitos reservados</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- SCRIPTS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-    crossorigin="anonymous"></script>
-</body>
-
-</html>
+  <?php get_footer(); ?>
