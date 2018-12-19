b43451d26d0200fe8a4a741486685b6be4a9bb8b
RyuMatador <sueliodez@hotmail.com>
2018-12-19 11:08:33 -0300

    Adiciona variavel nome do site

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Videira Palmas</title>
+    <title><?php bloginfo('name'); ?></title>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri()?>/icons/favicon.ico" type="image/ico">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
