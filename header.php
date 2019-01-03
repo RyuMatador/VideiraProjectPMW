24c38305da3a23c770fb230aedc0672b4c99077f
RyuMatador <sueliodez@hotmail.com>
2019-01-03 11:20:56 -0300

    Salvar

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 <!-- NAVIGATION -->
 <nav class="navbar navbar-expand-md nav-shadow fixed-top m-0 py-0 navbar-style">
         <div class="container">
-            <a href="index.html" class="navbar-brand">
+            <a href="index.php" class="navbar-brand">
             <?xml version="1.0" encoding="UTF-8" standalone="no"?>
                 <svg
                 xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -152,8 +152,8 @@
             <!-- Elementos escondidos na redução da largura da página -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto text-center text-md-right">
-                    <li class="nav-item"><a href="index.html" class="nav-link">Inicio</a></li>
-                    <li class="nav-item"><a href="pags/sobre.html" class="nav-link">Sobre</a></li>
+                    <li class="nav-item"><a href="<?php echo get_stylesheet_directory_uri()?>/index.php" class="nav-link">Inicio</a></li>
+                    <li class="nav-item"><a href="<?php echo get_stylesheet_directory_uri()?>pags/sobre.php" class="nav-link">Sobre</a></li>
                     <!-- Dropdown -->
                     <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" role="button" id="dropdownMenuLink"
                             data-toggle="dropdown">Células</a>
@@ -163,8 +163,8 @@
                             <a class="dropdown-item" href="#">Rede Adolescentes</a>
                         </div>
                     </li>
-                    <li class="nav-item"><a href="../palavras.html" class="nav-link">Palavras</a></li>
-                    <li class="nav-item"><a href="../contato.html" class="nav-link">Contato</a></li>
+                    <li class="nav-item"><a href="<?php echo get_stylesheet_directory_uri()?>/palavras.php" class="nav-link">Palavras</a></li>
+                    <li class="nav-item"><a href="<?php echo get_stylesheet_directory_uri()?>/contato.php" class="nav-link">Contato</a></li>
                 </ul>
             </div>
         </div>
